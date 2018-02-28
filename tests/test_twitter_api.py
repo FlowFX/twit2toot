05a8e1f704c677a3caa61cdd680a68bac3df013e
Florian Posdziech <hallo@flowfx.de>
2018-02-28 11:35:06 -0600

    Mock the Twitter API call

--- tests/test_twitter_api.py
+++ tests/test_twitter_api.py
@@ -1,10 +1,14 @@
 """Testing our usage of the Twitter API using tweepy."""
+from unittest.mock import patch
+
+import pytest
+
 from twit2toot import api
 
 from tweepy.models import Status
 
 
-
+@pytest.mark.skip(reason="This hits the Twitter API.")
 def test_get_user_timeline():
     """Exploratory test to get started with using tweepy."""
     # GIVEN any state
@@ -20,6 +24,24 @@ def test_get_user_timeline():
     assert isinstance(tweet, Status)
 
 
-def test_get_user_timeline_while_mocking_the_api_response():
-    """Rewrite the test above w/o hitting the Twitter API."""
-    pass
+@patch('tweepy.binder.requests.Session.request')
+def test_get_user_timeline_while_mocking_the_api_response(mock_response):
+    """Test correct usage of tweepy
+
+    Mocking the GET request to the Twitter API.
+    """
+    with open('tests/data/Twitter_User_Timeline-1519822598460.json') as f:
+        json_data = f.read()
+
+    mock_response.return_value.status_code = 200
+    mock_response.return_value.text = json_data
+
+    # GIVEN any state
+    # WHEN using the tweepy to get the user timeline from Twitter
+    response = api.user_timeline()
+
+    # THEN it's a list
+    assert isinstance(response, list)
+
+    # AND it's members are tweets
+    assert isinstance(response[0], Status)
