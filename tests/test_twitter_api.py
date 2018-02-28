a52d3957fbb6fc049fb7d6b401058363de8f5dc4
Florian Posdziech <hallo@flowfx.de>
2018-02-28 09:56:04 -0600

    Add test stubs

---
+++ tests/test_twitter_api.py
@@ -0,0 +1,11 @@
+"""Testing our usage of the Twitter API using tweepy."""
+
+
+def test_get_user_timeline():
+    """Exploratory test to get started with using tweepy."""
+    pass
+
+
+def test_get_user_timeline_while_mocking_the_api_response():
+    """Rewrite the test above w/o hitting the Twitter API."""
+    pass
