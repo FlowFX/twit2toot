3a803ede6a171c3f4baf3b80f5428b68bb5ed903
Florian Posdziech <hallo@flowfx.de>
2018-02-28 11:25:51 -0600

    Test tweepy hitting the Twitter API

--- tests/test_twitter_api.py
+++ tests/test_twitter_api.py
@@ -1,9 +1,23 @@
 """Testing our usage of the Twitter API using tweepy."""
+from twit2toot import api
+
+from tweepy.models import Status
+
 
 
 def test_get_user_timeline():
     """Exploratory test to get started with using tweepy."""
-    pass
+    # GIVEN any state
+    # WHEN using the tweepy to get the user timeline from Twitter
+    response = api.user_timeline(count=5)
+
+    # THEN it's a list of the correct length
+    assert isinstance(response, list)
+    assert len(response) == 5
+
+    # AND it's members are tweet objects
+    tweet = response[0]
+    assert isinstance(tweet, Status)
 
 
 def test_get_user_timeline_while_mocking_the_api_response():
---
+++ twit2toot/__init__.py
@@ -0,0 +1,23 @@
+import json
+
+import tweepy
+
+
+try:
+    with open('secrets.json') as f:
+        secrets = json.loads(f.read())
+except FileNotFoundError:
+    error_msg = 'secrets.json file is missing.'
+    raise FileNotFoundError(error_msg)
+
+
+auth = tweepy.OAuthHandler(
+    secrets['twitter']['consumer_key'],
+    secrets['twitter']['consumer_secret'],
+    )
+auth.set_access_token(
+    secrets['twitter']['access_token'],
+    secrets['twitter']['access_token_secret'],
+    )
+
+api = tweepy.API(auth)
