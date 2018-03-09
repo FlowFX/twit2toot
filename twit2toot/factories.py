45b2dbe82d0733e7460509a60afbed801238a877
Florian Posdziech <hallo@flowfx.de>
2018-03-08 19:46:23 -0600

    Add model factories
    UserFactory and StatusFactory to be used in testing. Model factories
    allow for easy creation of model objects with specific properties. We
    use Factory Boy for that (https://factoryboy.readthedocs.io/).

--- twit2toot/factories.py
+++ twit2toot/factories.py
@@ -13,11 +13,17 @@ class User(models.User):
 
 
 class Status(models.Status):
-    def __init__(self, id, created_at):
+    def __init__(self, id=None, created_at=None, author=None, text='', entities=[],
+                 in_reply_to_status_id=False, in_reply_to_user_id=False):
         super(Status, self).__init__()
 
         self.id = id
         self.created_at = created_at
+        self.author = author
+        self.text = text
+        self.entities = entities
+        self.in_reply_to_status_id = in_reply_to_status_id
+        self.in_reply_to_user_id = in_reply_to_user_id
 
 
 class UserFactory(factory.Factory):
@@ -33,15 +39,14 @@ class StatusFactory(factory.Factory):
 
     id = 628469339801452544
     created_at = datetime.datetime(2015, 8, 4, 7, 35, 41)
-    # # author = user
-    # text = 'Hallo. Ich bin ein Tweet.'
-    # entities = {
-    #     'hashtags': [],
-    #     'symbols': [],
-    #     'user_mentions': [],
-    #     'urls': [],
-    # }
-    # in_reply_to_screen_name = False
-    # in_reply_to_status_id = False
-    # in_reply_to_user_id = False
+    author = factory.SubFactory(UserFactory)
+    text = 'Hallo. Ich bin ein Tweet.'
+    entities: dict = {
+        'hashtags': [],
+        'symbols': [],
+        'user_mentions': [],
+        'urls': [],
+    }
+    in_reply_to_status_id = False
+    in_reply_to_user_id = False
     # is_quote_status = False
